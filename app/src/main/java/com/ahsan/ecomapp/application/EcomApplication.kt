6cdbca03d3139ebf6ef953d60c6a11682a5123a3
Syed Intiser Ahsan <Intiser@users.noreply.github.com>
2020-09-20 18:33:50 +0600

    update

---
+++ app/src/main/java/com/ahsan/ecomapp/application/EcomApplication.kt
@@ -0,0 +1,10 @@
+package com.ahsan.ecomapp.application
+
+import android.app.Application
+
+class EcomApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
